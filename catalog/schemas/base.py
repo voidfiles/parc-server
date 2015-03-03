05e375c838c7073ff50e49d9a56d6a51ade0db6b
Alex Kessinger <akessinger@bepress.com>
2015-03-02 21:46:09 -0800

    Custom datetime type

--- catalog/schemas/base.py
+++ catalog/schemas/base.py
@@ -1,8 +1,33 @@
+import datetime
+import iso8601
+
 from schematics.models import Model
+from schematics.types import BaseType
 
 PARC_ISO_STRF_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
-PARC_ISO_STRF_FORMATS = ('%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%dT%H:%M:%S.%f', '%Y-%m-%dT%H:%M:%S')
 
 class ParcSchema(Model):
     class Options:
         serialize_when_none = False
+
+
+
+class ISODateTimeType(BaseType):
+    MESSAGES = {
+        'parse': u'Could not parse {0}. Should be ISO8601.',
+    }
+
+
+    def to_native(self, value, context=None):
+        if isinstance(value, datetime.datetime):
+            return value
+
+        try:
+            return iso8601.parse_date(value)
+        except (ValueError, TypeError):
+            pass
+
+        raise ConversionError(self.messages['parse'].format(value))
+
+    def to_primitive(self, value, context=None):
+        return value.strftime(PARC_ISO_STRF_FORMAT)
